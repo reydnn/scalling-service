1256a01c30857d4ba1c1116357e3363668d32514
reydn <a.latypov@ylab.io>
2023-01-27 14:43:34 +0300

    add admins

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,1 +1,24 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from posts.models import Comment, Post, PostTag
+
+
+class PostTagInline(admin.TabularInline):
+    model = PostTag
+    fields = ["tag"]
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ["content", "author", "created_at"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = [
+        "title",
+        "content",
+        "user",
+        "likes",
+    ]
+    inlines = [PostTagInline, CommentInline]
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,39 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from users.models import Friendship, Interests, Photo, User, UserTag
+
+
+class InterestsInline(admin.TabularInline):
+    model = Interests
+    fields = ["interest"]
+
+
+class UserTagInline(admin.TabularInline):
+    model = UserTag
+    fields = ["tag"]
+
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    fields = ["url", "is_main"]
+
+
+class FriendshipInline(admin.TabularInline):
+    model = Friendship
+    fk_name = "user_1"
+    fields = ["user_1", "user_2", "request_date", "approve_date"]
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "city",
+        "gender",
+        "birthday",
+        "mobile_phone",
+    ]
+    list_filter = ["gender"]
+    inlines = [InterestsInline, UserTagInline, PhotoInline, FriendshipInline]
